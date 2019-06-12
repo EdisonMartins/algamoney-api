7709509fddd3cddf6c94a6b541b6606e0566ce93
Edison Martins <Edison Martins@DESKTOP-UBC9E7L>
2019-06-12 02:38:14 -0300

    Aula 5.6

--- algamoney-api/src/main/java/com/example/algamoney/api/service/PessoaService.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/service/PessoaService.java
@@ -33,7 +33,7 @@ public class PessoaService {
 		pessoaRepository.save(pessoaSalva);
 	}
 	
-	private Pessoa buscarPessoaPeloCodigo(Long codigo) {
+	public Pessoa buscarPessoaPeloCodigo(Long codigo) {
 		Optional<Pessoa> pessoaSalva = pessoaRepository.findById(codigo);
 		if (pessoaSalva.isEmpty()) {
 			throw new EmptyResultDataAccessException(1);
