83feb2f77526192dd756b7bc25441baf69f9d800
Edison Martins <martinsegmj@gmail.com>
2021-07-20 02:35:56 -0300

    7.5 - Nome do usuário no token JWT

--- algamoney-api/src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -6,12 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -20,14 +21,12 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
+import com.example.algamoney.api.config.token.CustomTokenEnhancer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter  {
-	
-	@Autowired
-	private UserDetailsService userDetailsService;
-	
+		
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
@@ -61,12 +60,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-	    endpoints
-	        .tokenStore(tokenStore())
-	        .accessTokenConverter(this.accessTokenConverter())
-	        .reuseRefreshTokens(false)
-	        .userDetailsService(this.userDetailsService)
-	        .authenticationManager(this.authenticationManager);
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+		
+		endpoints
+			.tokenStore(tokenStore())
+			.tokenEnhancer(tokenEnhancerChain)
+			.reuseRefreshTokens(false)
+			.authenticationManager(authenticationManager);
 	}	
 	
 	
@@ -94,5 +95,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
 	}	
+	
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+	    return new CustomTokenEnhancer();
+	}
 	
 }
