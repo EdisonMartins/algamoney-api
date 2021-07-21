0b138d17f4c96bd1c533684fe4eabc3e9f6948e5
Edison Martins <martinsegmj@gmail.com>
2021-07-21 00:46:53 -0300

    7.9 - Desafio: Atualização de lançamento <FIM_DO_CURSO>

--- algamoney-api/src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -2,6 +2,7 @@ package com.example.algamoney.api.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +14,11 @@ import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaExc
 
 @Service
 public class LancamentoService {
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
-	@Autowired 
+
+	@Autowired
 	private LancamentoRepository lancamentoRepository;
 
 	public Lancamento salvar(Lancamento lancamento) {
@@ -25,8 +26,38 @@ public class LancamentoService {
 		if (pessoa.isEmpty() || pessoa.get().isInativo()) {
 			throw new PessoaInexistenteOuInativaException();
 		}
-		
+
 		return lancamentoRepository.save(lancamento);
 	}
-	
+
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+
+	private void validarPessoa(Lancamento lancamento) {
+		Optional<Pessoa> pessoaOptional = null;
+		if (lancamento.getPessoa().getCodigo() != null) {
+			pessoaOptional = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+		}
+		
+		if (pessoaOptional.isEmpty() || pessoaOptional.get().isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+	}
+
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		Optional<Lancamento> lancamentoSalvoOptional = lancamentoRepository.findById(codigo);
+		if (lancamentoSalvoOptional.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		return lancamentoSalvoOptional.get();
+	}
+
 }
