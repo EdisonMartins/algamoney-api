adbb6ac4cd017468c3f8559853d1074edb1e4011
EdisonMartins <martinsegmj@gmail.com>
2023-10-02 23:52:19 -0300

    Adicionado OauthSecurityConfig para adicionar o bean authenticationManager

--- algamoney-api/src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -11,17 +11,12 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
 import com.example.algamoney.api.config.token.CustomTokenEnhancer;
 
 @Profile("oauth-security")
@@ -31,9 +26,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
-	@Autowired
-	private AlgamoneyApiProperty algamoneyApiProperty; 
+
+// Utilizado no Cors Filter	
+//	@Autowired
+//	private AlgamoneyApiProperty algamoneyApiProperty; 
 	
 
 	@Override
@@ -43,7 +39,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.secret("$2a$10$gY96zbUSj.nUmJfwTX2a7O6NO0TcJ/Me/BAogErwUkzi3c1FsNNJe")
 				.scopes("read", "write")
 				.authorizedGrantTypes("password", "refresh_token")
-				
+				.accessTokenValiditySeconds(1800)
 				.refreshTokenValiditySeconds(3600 * 24)
 			.and()
 				.withClient("mobile")
@@ -52,12 +48,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.authorizedGrantTypes("password", "refresh_token")
 				.accessTokenValiditySeconds(1800)
 				.refreshTokenValiditySeconds(3600 * 24);
-	}
-	
-	
-	@Override
-	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		security.addTokenEndpointAuthenticationFilter(this.corsFilter());
 	}
 	
 	@Override
@@ -72,7 +62,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.authenticationManager(authenticationManager);
 	}	
 	
-	
 	@Bean
 	public TokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
@@ -85,22 +74,38 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		return accessTokenConverter;
 	}
 	
-	@Bean
-	public CorsFilter corsFilter() {
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		config.setMaxAge(3600L);
-		config.setAllowedOrigins(Arrays.asList(algamoneyApiProperty.getOriginPermitida(), "http://localhost:4200"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}	
 	
 	@Bean
 	public TokenEnhancer tokenEnhancer() {
 	    return new CustomTokenEnhancer();
 	}
+	
+	
+
+	
+//************************************************* Cors Filter ***************************************************	
+//*****************************************************************************************************************	
+//	@Override
+//	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+//		security.addTokenEndpointAuthenticationFilter(this.corsFilter());
+//	}
+//	
+//
+//
+//	@Bean
+//	public CorsFilter corsFilter() {
+//		CorsConfiguration config = new CorsConfiguration();
+//		config.setAllowCredentials(true);
+//		config.addAllowedHeader("*");
+//		config.addAllowedMethod("*");
+//		config.setMaxAge(3600L);
+//		config.setAllowedOrigins(Arrays.asList(algamoneyApiProperty.getOriginPermitida(), "http://localhost:4200"));
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", config);
+//		return new CorsFilter(source);
+//	}	
+	//*****************************************************************************************************************	
+	
+
 	
 }
